62473bc3ca9fdf3685edc39c6b0dd294616b43e7
hsshin <hsshin@digitalzone.co.kr>
2023-05-04 08:49:39 +0900

    토큰 컴증 test 수정

--- src/test/java/com/bank/core/jwt/JwtProcessTest.java
+++ src/test/java/com/bank/core/jwt/JwtProcessTest.java
@@ -13,6 +13,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JwtProcessTest {
 
+    private static String createToken() {
+        User user = User
+                .builder()
+                .id(1L)
+                .role(UserEnum.CUSTOMER)
+                .build();
+
+        LoginUser loginUser = new LoginUser(user);
+        String jwtToken = JwtProcess.create(loginUser);
+        return jwtToken;
+    }
+
     @Test
     @DisplayName("토큰 생성")
     void jwtCreate_test() {
@@ -31,7 +43,9 @@ class JwtProcessTest {
     @Test
     @DisplayName("토큰 검증")
     void jwtVerify_test() {
-        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY4MjMzNjgxOX0.LGkhXitgghy7pu6DPU0mmv9Pd2jS3lVGhzYNPY19Ik9dXwrcX80NRRVNqHcy_EYG47v2_vi3N4ttmGhDaMCukg";
+        String jwtToken = createToken();
+        jwtToken = jwtToken.replace(JwtVO.TOKEN_PREFIX, "");
+
         LoginUser loginUser = JwtProcess.verify(jwtToken);
         assertThat(loginUser.getUser().getId()).isEqualTo(1L);
         assertThat(loginUser.getUser().getRole()).isEqualTo(UserEnum.CUSTOMER);
